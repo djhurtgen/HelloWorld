3a48eddaf9ad0269fbc735c45be264e507fb365e
djhurtgen <dhurtgen@fredonia.edu>
2022-10-17 11:18:17 -0400

    first commit

---
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+<<<<<<< HEAD
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.listCity = new System.Windows.Forms.ListBox();
+            this.checkLive = new System.Windows.Forms.CheckBox();
+            this.checkDie = new System.Windows.Forms.CheckBox();
+            this.bttnSubmit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+=======
+            this.label2 = new System.Windows.Forms.Label();
+            this.listCity = new System.Windows.Forms.ListBox();
+            this.rdMale = new System.Windows.Forms.RadioButton();
+            this.rdFemale = new System.Windows.Forms.RadioButton();
+            this.checkLive = new System.Windows.Forms.CheckBox();
+            this.checkDie = new System.Windows.Forms.CheckBox();
+            this.bttnSubmit = new System.Windows.Forms.Button();
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.textAddress);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.rdFemale);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.rdMale);
+            this.groupBox1.Location = new System.Drawing.Point(32, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(383, 166);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User Details";
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(113, 76);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(181, 22);
+            this.textAddress.TabIndex = 4;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(113, 36);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(181, 22);
+            this.textName.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Address";
+            // 
+<<<<<<< HEAD
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(6, 130);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(74, 20);
+            this.rdFemale.TabIndex = 6;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+=======
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+<<<<<<< HEAD
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(6, 104);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(58, 20);
+            this.rdMale.TabIndex = 5;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+=======
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            // listCity
+            // 
+            this.listCity.FormattingEnabled = true;
+            this.listCity.ItemHeight = 16;
+            this.listCity.Items.AddRange(new object[] {
+            "Dunkirk",
+            "Fredonia",
+            "Rochester",
+            "Da Bronx"});
+            this.listCity.Location = new System.Drawing.Point(32, 236);
+            this.listCity.Name = "listCity";
+            this.listCity.Size = new System.Drawing.Size(120, 84);
+            this.listCity.TabIndex = 4;
+            this.listCity.SelectedIndexChanged += new System.EventHandler(this.listCity_SelectedIndexChanged);
+            // 
+<<<<<<< HEAD
+=======
+            // rdMale
+            // 
+            this.rdMale.AutoSize = true;
+            this.rdMale.Location = new System.Drawing.Point(6, 104);
+            this.rdMale.Name = "rdMale";
+            this.rdMale.Size = new System.Drawing.Size(58, 20);
+            this.rdMale.TabIndex = 5;
+            this.rdMale.TabStop = true;
+            this.rdMale.Text = "Male";
+            this.rdMale.UseVisualStyleBackColor = true;
+            // 
+            // rdFemale
+            // 
+            this.rdFemale.AutoSize = true;
+            this.rdFemale.Location = new System.Drawing.Point(6, 130);
+            this.rdFemale.Name = "rdFemale";
+            this.rdFemale.Size = new System.Drawing.Size(74, 20);
+            this.rdFemale.TabIndex = 6;
+            this.rdFemale.TabStop = true;
+            this.rdFemale.Text = "Female";
+            this.rdFemale.UseVisualStyleBackColor = true;
+            // 
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            // checkLive
+            // 
+            this.checkLive.AutoSize = true;
+            this.checkLive.Location = new System.Drawing.Point(482, 142);
+            this.checkLive.Name = "checkLive";
+            this.checkLive.Size = new System.Drawing.Size(54, 20);
+            this.checkLive.TabIndex = 7;
+            this.checkLive.Text = "Live";
+            this.checkLive.UseVisualStyleBackColor = true;
+            // 
+            // checkDie
+            // 
+            this.checkDie.AutoSize = true;
+            this.checkDie.Location = new System.Drawing.Point(482, 184);
+            this.checkDie.Name = "checkDie";
+            this.checkDie.Size = new System.Drawing.Size(50, 20);
+            this.checkDie.TabIndex = 8;
+            this.checkDie.Text = "Die";
+            this.checkDie.UseVisualStyleBackColor = true;
+            // 
+            // bttnSubmit
+            // 
+            this.bttnSubmit.Location = new System.Drawing.Point(32, 355);
+            this.bttnSubmit.Name = "bttnSubmit";
+            this.bttnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.bttnSubmit.TabIndex = 9;
+            this.bttnSubmit.Text = "Submit";
+            this.bttnSubmit.UseVisualStyleBackColor = true;
+            this.bttnSubmit.Click += new System.EventHandler(this.bttnSubmit_Click);
+            // 
+<<<<<<< HEAD
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.groupBox2.Location = new System.Drawing.Point(232, 236);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(183, 203);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nothing to see here.";
+            // 
+=======
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 504);
+<<<<<<< HEAD
+            this.Controls.Add(this.groupBox2);
+=======
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+            this.Controls.Add(this.bttnSubmit);
+            this.Controls.Add(this.checkDie);
+            this.Controls.Add(this.checkLive);
+            this.Controls.Add(this.listCity);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listCity;
+        private System.Windows.Forms.RadioButton rdMale;
+        private System.Windows.Forms.RadioButton rdFemale;
+        private System.Windows.Forms.CheckBox checkLive;
+        private System.Windows.Forms.CheckBox checkDie;
+        private System.Windows.Forms.Button bttnSubmit;
+<<<<<<< HEAD
+        private System.Windows.Forms.GroupBox groupBox2;
+=======
+>>>>>>> 903298537cf1a2bf4a41ad08fdd1c045022535ea
+    }
+}
+
---
+++ HelloWorld/HelloWorld/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string text = listCity.GetItemText(listCity.SelectedItem);
+            MessageBox.Show(text);
+        }
+
+        private void bttnSubmit_Click(object sender, EventArgs e)
+        {
+            String name = textName.Text;
+            String address = textAddress.Text;
+            MessageBox.Show(name + " " + address);
+        }
+    }
+}
